29e14b0bbb9b7ac8ce72132a234179a85356015a
Rohan Lad <rohlad@WKMZT248D039.global.publicisgroupe.net>
2022-10-31 15:35:42 +0000

    module1 done

--- module1/my-app/src/App.tsx
+++ module1/my-app/src/App.tsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 function App() {
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      number: '',
+    },
+    onSubmit: function (values) {
+      alert(`You are registered! Name: ${values.name}. Email: ${values.email}.
+        Phone Number: ${values.number}`);
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      number: Yup.string()
+        .matches(/^(07\d{8,12}|447\d{7,11})$/, 'Phone number is not valid')
+        .required()
+    })
+
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <form onSubmit={formik.handleSubmit}>
+        <h1 >Register</h1>
+        <div >
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name"
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
+          {formik.touched.name && formik.errors.name && (
+            <span>{formik.errors.name}</span>
+          )}
+        </div>
+        <div >
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email"
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
+          {formik.touched.email && formik.errors.email && (
+            <span >{formik.errors.email}</span>
+          )}
+        </div>
+        <div >
+          <label htmlFor="number">Phone Number</label>
+          <input type="text" name="number" id="number"
+            onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.number} />
+          {formik.touched.number && formik.errors.number && (
+            <span >{formik.errors.number}</span>
+          )}
+        </div>
+        <div >
+          <button type='submit'>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
