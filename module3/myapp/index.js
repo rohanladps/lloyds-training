6fc8faeee9ef562059890f52c61ba003d3fe946f
Rohan Lad <rohlad@WKMZT248D039.global.publicisgroupe.net>
2022-11-02 13:27:35 +0000

    module3 done

--- module3/myapp/index.js
+++ module3/myapp/index.js
@@ -4,22 +4,33 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 const path = require('path');
+const winston = require('winston');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
+const logConfiguration = {
+    'transports': [
+        new winston.transports.Console()
+    ]
+};
+
+const logger = winston.createLogger(logConfiguration);
 
 app.use(cors());
-
-// Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
 app.post('/submit', (req, res) => {
-    const book = req.body;
+    logger.log({
+        message: 'Hello, Winston!',
+        level: 'info'
+    });
+    logger.info('Hello, Winston!');
     res.send('The server has successfully received your request!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
