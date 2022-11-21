2fa23adb469a0a7d2e66370ad1dca64734d9033a
Rohan Lad <rohlad@WKMZT248D039.global.publicisgroupe.net>
2022-11-21 11:21:01 +0000

    use of writeheapsnapshot

--- module12/typescript-starter/build/index.js
+++ module12/typescript-starter/build/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var dotenv_1 = __importDefault(require("dotenv"));
+var v8_1 = require("v8");
 dotenv_1.default.config();
 var app = (0, express_1.default)();
 var port = 3000;
@@ -15,12 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 var fs = require('fs');
 var v8 = require('v8');
-function createHeapSnapshot() {
-    var snapshotStream = v8.getHeapSnapshot();
-    var fileName = "".concat(Date.now(), ".heapsnapshot");
-    var fileStream = fs.createWriteStream(fileName);
-    snapshotStream.pipe(fileStream);
-}
 var Car = /** @class */ (function () {
     function Car(model, engine, colour) {
         this.model = model;
@@ -29,17 +24,17 @@ var Car = /** @class */ (function () {
     }
     return Car;
 }());
-createHeapSnapshot();
+(0, v8_1.writeHeapSnapshot)();
 var car1 = new Car('bmw', '1000', 'blue');
 var car2 = new Car('honda', '2000', 'green');
 var car3 = new Car('fiat', '3000', 'red');
-createHeapSnapshot();
+(0, v8_1.writeHeapSnapshot)();
 var car4 = new Car('merc', '4000', 'yellow');
 var car5 = new Car('porsche', '5000', 'white');
-createHeapSnapshot();
+(0, v8_1.writeHeapSnapshot)();
 // @ts-ignore
 car3 = undefined;
-createHeapSnapshot();
+(0, v8_1.writeHeapSnapshot)();
 app.listen(port, function () {
     console.log("\u26A1\uFE0F[server]: Server is running at https://localhost:".concat(port));
 });
--- module12/typescript-starter/src/index.ts
+++ module12/typescript-starter/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { writeHeapSnapshot } from 'v8';
 
 dotenv.config();
 const app: Express = express();
@@ -11,13 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const fs = require('fs');
 const v8 = require('v8');
-
-function createHeapSnapshot() {
-    const snapshotStream = v8.getHeapSnapshot();
-    const fileName = `${Date.now()}.heapsnapshot`;
-    const fileStream = fs.createWriteStream(fileName);
-    snapshotStream.pipe(fileStream);
-}
 
 interface Car {
     model: string,
@@ -33,21 +27,21 @@ class Car {
     }
 }
 
-createHeapSnapshot()
+writeHeapSnapshot()
 
 var car1 = new Car('bmw', '1000', 'blue');
 var car2 = new Car('honda', '2000', 'green');
 var car3 = new Car('fiat', '3000', 'red');
-createHeapSnapshot()
+writeHeapSnapshot()
 var car4 = new Car('merc', '4000', 'yellow');
 var car5 = new Car('porsche', '5000', 'white');
 
-createHeapSnapshot()
+writeHeapSnapshot()
 
 // @ts-ignore
 car3 = undefined;
 
-createHeapSnapshot()
+writeHeapSnapshot()
 
 
 app.listen(port, () => {
